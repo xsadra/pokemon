d10adbd1cf7efbee2b6edd15a798707f885cfede
Sadra <31988724+xsadra@users.noreply.github.com>
2022-05-17 14:22:33 +0200

    Create App class to contain texts

--- lib/core/constants/consts.dart
+++ lib/core/constants/consts.dart
@@ -1,7 +1,37 @@
-class Consts {
-  static String pokemonListApiUrl =
-      'https://pokeapi.co/api/v2/pokemon?limit=151';
-  static String shapesBaseApiUrl = 'https://pokeapi.co/api/v2/pokemon-shape/';
-  static String speciesBaseApiUrl =
-      'https://pokeapi.co/api/v2/pokemon-species/';
+class App {
+  static ApiAppTexts api = const ApiAppTexts();
+  static ErrorAppTexts error = const ErrorAppTexts();
+  static UIAppTexts ui = const UIAppTexts();
+
+  App._();
+}
+
+class AppRouteTexts {
+  AppRouteTexts();
+
+  static const DETAIL_PAGE_ROUTE = 'DetailPageRoute';
+  static const POKEMON_PAGE_ROUTE = 'PokemonPageRoute';
+}
+
+class ErrorAppTexts {
+  const ErrorAppTexts();
+
+  String get serverFailureMessage => 'Server Failure';
+  String get notFoundFailureMessage => 'Set your player Tag: #xxxxxxx';
+  String get unexpectedError => 'Unexpected error';
+  String get unexpectedEvent => 'Unexpected event';
+}
+
+class ApiAppTexts {
+  const ApiAppTexts();
+
+  String get pokemonListApiUrl => 'https://pokeapi.co/api/v2/pokemon?limit=151';
+  String get shapesBaseApiUrl => 'https://pokeapi.co/api/v2/pokemon-shape/';
+  String get speciesBaseApiUrl => 'https://pokeapi.co/api/v2/pokemon-species/';
+}
+
+class UIAppTexts {
+  const UIAppTexts();
+
+  String get title => 'Pokemon Game';
 }
