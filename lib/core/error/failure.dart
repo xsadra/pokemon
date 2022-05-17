06483e5beb48647f7c48b2db7d73082ae604dbe3
Sadra <31988724+xsadra@users.noreply.github.com>
2022-05-17 14:29:18 +0200

    Add Extension to Failure class to clarifying Failure Messages

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,3 +1,5 @@
+import '../constants/consts.dart';
+
 abstract class Failure {}
 
 // General failures
@@ -6,3 +8,16 @@ class ServerFailure extends Failure {}
 class NotFoundFailure extends Failure {}
 
 class NoInternetFailure extends Failure {}
+
+extension FailureExtension on Failure {
+  String get toMessage {
+    switch (runtimeType) {
+      case ServerFailure:
+        return App.error.serverFailureMessage;
+      case NotFoundFailure:
+        return App.error.notFoundFailureMessage;
+      default:
+        return App.error.unexpectedError;
+    }
+  }
+}
