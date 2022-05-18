31b2457f3ecef1e58f35405a4cf8eb551dcbd0b1
Sadra <31988724+xsadra@users.noreply.github.com>
2022-05-18 18:19:21 +0200

    Finish main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart' hide Router;
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'core/core.dart';
 import 'core/routes/router.dart';
-import 'data/repository/pokemon_list_repository.dart';
-import 'domain/entities/Pokemon.dart';
 import 'injection_container.dart' as injection;
+import 'presentation/bloc/pokemon/bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,123 +22,14 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final _router = Router();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: _router.defaultRouteParser(),
-      routerDelegate: _router.delegate(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Pokemon> items = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pokemon'),
-      ),
-      body: Scrollbar(
-        child: items.isEmpty
-            ? Center(
-                child: Column(
-                children: [
-                  const CircularProgressIndicator(),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {});
-                      },
-                      child: const Text('Load'))
-                ],
-              ))
-            : ListView.builder(
-                // controller: controller,
-                itemBuilder: (context, index) {
-                  var item = items[index];
-                  //
-                  return ListTile(
-                    title: Text(item.name),
-                    subtitle: Text(item.id.toString()),
-                    leading: Image.network(item.image!),
-                    onTap: () => _onTap(item),
-                  );
-                },
-                itemCount: items.length,
-              ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  @override
-  void initState() {
-    _incrementCounter();
-  }
-
-  _incrementCounter() async {
-    log('Start');
-    items = await injection.sl<PokemonRepository>().getPokemons();
-    log('Set State');
-    setState(() {});
-  }
-
-  void _onTap(Pokemon item) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => Detail(item: item)),
-    );
-  }
-}
-
-class Detail extends StatelessWidget {
-  Detail({Key? key, required this.item}) : super(key: key);
-  final Pokemon item;
-
-  @override
-  Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height / 3;
-    return Scaffold(
-      appBar: AppBar(title: Text(item.name)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(height: height, child: Image.network(item.image!)),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [const Text('Name:'), Text(item.name)]),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [const Text('Id:'), Text(item.id.toString())]),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              const Text('Height:'),
-              Text(item.height.toString())
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              const Text('Weight:'),
-              Text(item.weight.toString())
-            ]),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [const Text('Order:'), Text(item.order.toString())]),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [const Text('Species:'), Text(item.species ?? '')]),
-            SizedBox(
-              height: height,
-            )
-          ],
-        ),
+    return BlocProvider(
+      create: (context) => injection.sl<PokemonBloc>()..add(GetPokemonEvent()),
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routeInformationParser: _router.defaultRouteParser(),
+        routerDelegate: _router.delegate(),
       ),
     );
   }
