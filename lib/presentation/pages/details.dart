e78553b9e9eecf9546620696579daa91f4a5ac82
Sadra <31988724+xsadra@users.noreply.github.com>
2022-05-18 18:18:15 +0200

    Finish details Page

--- lib/presentation/pages/details.dart
+++ lib/presentation/pages/details.dart
@@ -1,21 +1,53 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../domain/entities/entities.dart';
+import '../widgets/widgets.dart';
+
 class DetailsPage extends StatelessWidget {
-  const DetailsPage({Key? key}) : super(key: key);
+  const DetailsPage({Key? key, required this.pokemon}) : super(key: key);
+
+  final Pokemon pokemon;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          const Text('Details'),
-          ElevatedButton(
-              onPressed: () {
-                context.router.pop();
-              },
-              child: Icon(Icons.skip_previous))
-        ],
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(title: Text(pokemon.name)),
+      body: SizedBox(
+        height: height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: height / 4,
+                  child: Image.network(
+                    pokemon.image!,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SizedBox(
+                  height: height / 12,
+                  child: Text(pokemon.name,
+                      style: TextStyle(
+                          color: Colors.green.shade800,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold)),
+                ),
+                ShowText(title: 'Id', value: pokemon.id.toString()),
+                ShowText(title: 'Order', value: pokemon.order.toString()),
+                ShowText(title: 'Height', value: pokemon.height.toString()),
+                ShowText(title: 'Weight', value: pokemon.weight.toString()),
+                ShowText(title: 'Species', value: pokemon.species.toString()),
+                ShowText(
+                    title: 'Base Ex', value: pokemon.baseExperience.toString()),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
