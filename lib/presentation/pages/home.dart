0e4e7f73eac9abc49a33b0e54624b65ff16e504f
Sadra <31988724+xsadra@users.noreply.github.com>
2022-05-18 15:16:44 +0200

    Show Item names and load more by scrolling

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,25 +16,7 @@ class HomePage extends StatelessWidget {
       appBar: _buildAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: BlocBuilder<PokemonBloc, PokemonState>(
-          builder: (context, state) {
-            if (state is Loaded) {
-              Pokemons pokemons = state.pokemons;
-              List<Pokemon> items = pokemons.items;
-              return ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  Pokemon pokemon = items[index];
-                  return Text(pokemon.name);
-                },
-              );
-            } else if (state is Error) {
-              return Center(child: Text(state.message));
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
+        child: PokemonListView(context),
       ),
     );
   }
@@ -40,6 +24,102 @@ class HomePage extends StatelessWidget {
   AppBar _buildAppBar() {
     return AppBar(
       title: Text(App.ui.appTitle),
+    );
+  }
+}
+
+class PokemonListView extends StatelessWidget {
+  PokemonListView(BuildContext context, {Key? key}) : super(key: key);
+  final _scrollController = ScrollController();
+
+  void _setupScrollController(BuildContext context) {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        context.read<PokemonBloc>().add(GetPokemonEvent());
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _setupScrollController(context);
+    return BlocBuilder<PokemonBloc, PokemonState>(
+      builder: (context, state) {
+        logShort.wtf(state.runtimeType, PokemonListView);
+        if (state is Loaded) {
+          Pokemons pokemons = state.pokemons;
+          List<Pokemon> items = pokemons.items;
+
+          return _buildListView2(items);
+        } else if (state is Error) {
+          return Center(child: Text(state.message));
+        } else if (state is Loading) {
+          return state.isFirstFetch
+              ? const Center(child: CircularProgressIndicator())
+              : _buildListView2(state.pokemons, isLoading: true);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  ListView _buildListView2(
+    List<Pokemon> items, {
+    bool isLoading = false,
+  }) {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: isLoading ? items.length + 1 : items.length,
+      itemBuilder: (context, index) {
+        if (isLoading && items.length == index) {
+          Timer(
+              const Duration(milliseconds: 20),
+              () => _scrollController
+                  .jumpTo(_scrollController.position.maxScrollExtent));
+          return const Center(child: CircularProgressIndicator());
+        }
+        Pokemon pokemon = items[index];
+        return Padding(
+          padding: const EdgeInsets.all(00.0),
+          child: Text(pokemon.name),
+        );
+      },
+    );
+  }
+
+  ListView _buildListView(
+    List<Pokemon> items, {
+    bool isLoading = false,
+    required ScrollController controller,
+  }) {
+    return ListView.builder(
+      controller: controller,
+      itemCount: isLoading ? items.length + 1 : items.length,
+      itemBuilder: (context, index) {
+        if (isLoading && items.length == index) {
+          Timer(
+              const Duration(milliseconds: 20),
+              () => _scrollController
+                  .jumpTo(_scrollController.position.maxScrollExtent));
+          return const Center(child: CircularProgressIndicator());
+        }
+        Pokemon pokemon = items[index];
+        return Padding(
+          padding: const EdgeInsets.all(00.0),
+          child: Row(
+            children: [
+              Text(pokemon.name),
+              ElevatedButton(
+                  onPressed: () {
+                    context.read<PokemonBloc>().add(GetPokemonEvent());
+                  },
+                  child: const Icon(Icons.add))
+            ],
+          ),
+        );
+      },
     );
   }
 }
